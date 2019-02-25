9b899cf1a45fb80cd33e414dd9c9036ca39cb078
lcohio <coninglogan@gmail.com>
2019-02-24 19:41:17 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const ctx = document.getElementById('traffic-widget').getContext('2d');
+const ctx2 = document.getElementById('daily-traffic-widget').getContext('2d');
+
+const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: '#7B72D8',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+const chart2 = new Chart(ctx2, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: '#7B72D8',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
