f03acecd9ba05efec76573156f3874795d803808
lcohio <coninglogan@gmail.com>
2019-02-25 21:52:41 -0500

    Progress commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const ctx = document.getElementById('traffic-widget').getContext('2d');
 const ctx2 = document.getElementById('daily-traffic-widget').getContext('2d');
+const ctx3 = document.getElementById('mobile-users-widget').getContext('2d');
 
 const chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -39,3 +40,21 @@ const chart2 = new Chart(ctx2, {
     options: {}
 });
 
+const chart3 = new Chart(ctx3, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: '#7B72D8',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
