5df69165bc86ca08a96cef31dc4a30b85ba3e9d2
lcohio <coninglogan@gmail.com>
2019-03-02 20:18:12 -0500

    Adjust grid layout for charts, add readme.md

--- js/app.js
+++ js/app.js
@@ -1,29 +1,19 @@
-const ctx = document.getElementById('traffic-widget').getContext('2d');
-const ctx2 = document.getElementById('daily-traffic-widget').getContext('2d');
-const ctx3 = document.getElementById('mobile-users-widget').getContext('2d');
+const traffic = document.getElementById('traffic-widget').getContext('2d');
+const dailyTraffic = document.getElementById('daily-traffic-widget').getContext('2d');
+const mobileUsers = document.getElementById('mobile-users-widget').getContext('2d');
+const alertNotify = document.querySelector('.alert-demo');
+const alertButton = document.querySelector('#closealert');
+const sendButton = document.querySelector('#submit-form');
+const textField = document.querySelector('#message-form textarea');
+const searchUser = document.getElementById('user-search');
 
-const chart = new Chart(ctx, {
+
+
+
+
+const chart = new Chart(traffic, {
     // The type of chart we want to create
     type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
-            label: "Monthly Traffic",
-            backgroundColor: '#75b5ff',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
-const chart2 = new Chart(ctx2, {
-    // The type of chart we want to create
-    type: 'bar',
 
     // The data for our dataset
     data: {
@@ -40,7 +30,26 @@ const chart2 = new Chart(ctx2, {
     options: {}
 });
 
-const chart3 = new Chart(ctx3, {
+const chart2 = new Chart(dailyTraffic, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "Monthly Traffic",
+            backgroundColor: '#7B72D8',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [2, 5, 9, 13, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+const chart3 = new Chart(mobileUsers, {
     // The type of chart we want to create
     type: 'doughnut',
 
@@ -59,10 +68,20 @@ const chart3 = new Chart(ctx3, {
     options: {}
 });
 
+// Hide alert bar when user clicks the close button
 
-const alertNotify = document.querySelector('.alert-demo');
-const alertButton = document.querySelector('#closealert');
-
-alertButton.addEventListener('click', () => {
+alertButton.addEventListener('click', (e) => {
     alertNotify.style.display = 'none';
+    e.preventDefault();
+});
+
+// Handle user messaging. If either "search" or "message" field is blank, display a warning. Otherwise, notify user the message was sent
+
+sendButton.addEventListener('click', (e) => {
+    if (textField.value === "" || searchUser.value === "") {
+        e.preventDefault();
+        alert('Please complete both fields.')
+    } else {
+        alert('Your message has been successfully sent!')
+    }
 });
---
+++ readme.md
@@ -0,0 +1,5 @@
+Web App Dashboard Project #7 as part of the TechDegree. 
+
+3/2/2019
+
+By: Logan Coning
